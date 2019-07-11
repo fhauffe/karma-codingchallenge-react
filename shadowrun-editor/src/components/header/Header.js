9bf9e8541b8b5c4158eced57797e3b3d1d46c173
fhauffe <f.hauffe@imia.de>
2019-07-11 11:01:36 +0200

    [TASK] Implemented header image

--- shadowrun-editor/src/components/header/Header.js
+++ shadowrun-editor/src/components/header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import logo from './header-banner.jpg'
 
 class Header extends React.Component {
   render() {
     return (
-      <div className="header-image"></div>
+      <div><img className="header-image" src={logo} alt="Logo" /></div>
     );
   }
 }
